6a6c245a406bdaac3dd9318f1bbf589faf2a3098
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-07-14 18:54:41 +0530

    ADD: dbcofig file

---
+++ src/dbconfig/dbConfig.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function connect(){
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI!);
+        const connection = mongoose.connection;
+
+        connection.on("connected", ()=> {
+            console.log("Connected to MongoDB Successfully");
+        })
+
+        connection.on("error", (err) => {
+            console.log("Error while connecting to MongoDB", err);
+            process.exit()
+        })
+        
+    } catch (error) {
+        console.log("Something went Wrong while connection with MongoDB", error);
+        
+    }
+}

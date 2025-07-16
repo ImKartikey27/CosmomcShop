778db53d8987239198a1ccf0fb73445611839cdb
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-07-16 15:38:20 +0530

    ADD: Product routes

---
+++ src/app/api/user/product/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import {connect} from "@/dbconfig/dbConfig"
+import Product from "@/models/product.js"
+
+connect()
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {id} = reqBody
+        const product = await Product.findById(id)
+        if(!product){
+            return NextResponse.json({
+                message: "Product not found",
+                status: 404
+            })
+        }
+        return NextResponse.json({
+            product,
+            message: "Product Fetched Successfully",
+            status: 200
+        })
+        
+    } catch (error:any) {
+        return NextResponse.json({
+            message: error.message,
+            status: 500
+        })
+    }
+    
+}
---
+++ src/app/api/user/products/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import {connect} from "@/dbconfig/dbConfig"
+import Product from "@/models/product.js";
+
+connect()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json();
+        const { category } = reqBody;
+        
+        const validCategories = ["coin", "rank", "crate"];
+        
+        if (!category) {
+            return NextResponse.json({
+                message: "Category is required",
+                status: 400
+            });
+        }
+        
+        if (!validCategories.includes(category)) {
+            return NextResponse.json({
+                message: "Invalid category",
+                status: 400
+            });
+        }
+
+        const products = await Product.find({ category });
+        
+        if (products.length === 0) {
+            return NextResponse.json({
+                message: "No products found for this category",
+                status: 404
+            });
+        }
+
+        return NextResponse.json({
+            products,
+            message: "Products Fetched Successfully",
+            status: 200
+        });
+        
+    } catch (error: any) {
+        return NextResponse.json({
+            message: error.message,
+            status: 500
+        });
+    }
+}

89285a3708b0a710ee36adb6b198d9d34b486486
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-07-16 15:19:33 +0530

    ADD: Schema Design

--- src/models/admin.js
+++ src/models/admin.js
@@ -17,8 +17,7 @@ const adminSchema = new mongoose.Schema({
         required: true
     },
     refreshToken: {
-        type: String, 
-        required: true
+        type: String
     },
     forgetPasswordToken: String, 
     forgetPasswordExpiry: Date
---
+++ src/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true
+    },
+    transactionHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Order"
+        }
+    ],
+    token: {
+        type: String
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema)
+export default User;

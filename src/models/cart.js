c9e54fa86885ca91205c7910519d63e80e6b21e5
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-07-17 20:11:17 +0530

    ADD: cart schema

---
+++ src/models/cart.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    products:[
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    totalAmount: {
+        type: Number,
+        required: true
+    }
+},{timestamps:true})
+
+const Cart = mongoose.models.Cart || mongoose.model("Cart", cartSchema)
+export default Cart
--- src/models/order.js
+++ src/models/order.js
@@ -6,25 +6,9 @@ const orderSchema = new mongoose.Schema({
         ref:"User",
         required: true
     },
-    products: [
-        {
-            productId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: true
-            },
-            quantity:{
-                type: Number,
-                required: true
-            },
-            price:{
-                type: Number,
-                required: true
-            }
-        }
-    ],
-    totalAmount:{
-        type: Number,
+    cart:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Cart",
         required: true
     },
     status: {

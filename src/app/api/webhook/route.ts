ab725463d0c7ebc0738794e8c9ff967cc26867c5
hproooo55 <roshni25dec@gmail.com>
2025-07-20 12:28:11 +0530

    updated track-invites route

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,55 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-    interface WebhookRequest {
-        id: string;
-        type: string;
-        date: string;
-        subject: {
-            transaction_id: string;
-            status: { id: number; description: string; };
-            payment_sequence: string;
-            created_at: string;
-            price: {
-                amount: number;
-                currency: string;
-                base_currency: string;
-                base_currency_price: number;
-            };
-            price_paid: {
-                amount: number;
-                currency: string;
-                base_currency: string;
-                base_currency_price: number;
-            };
-            payment_method: { name: string; refundable: boolean; };
-            fees: { tax: object; gateway: object; };
-            customer: {
-                first_name: string;
-                last_name: string;
-                email: string;
-                ip: string;
-                username: object;
-                marketing_consent: boolean;
-                country: string;
-                postal_code: string | null;
-            };
-            products: object[];
-            coupons: any[];
-            gift_cards: any[];
-            recurring_payment_reference: string | null;
-            custom: object;
-            revenue_share: any[];
-            decline_reason: string | null;
-            creator_code: string | null;
-        }
-    }
+    
 
 
 try {
 
     // Verify the signature
-    const reqBody: WebhookRequest = await req.json()
+    const reqBody = await req.json()
     const { id, type, subject } = reqBody
     if (!id || !type) {
         return NextResponse.json({ error: 'Invalid request body' }, { status: 400 });
